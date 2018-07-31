c924b756ca1eff27727de5bf9185fcd547c40ff1
Betsey R <betsey.r@gmail.com>
2018-07-31 22:08:47 +0800

    exercises

---
+++ the_basics_exercises_2.rb
@@ -0,0 +1,6 @@
+# let the number be 4567
+thousand = 4567/1000
+hundred = 4547%1000/100
+tens = 4547%1000%100/10
+ones = 4547%1000%100%10
+puts "thousand #{thousand} hundreds #{hundred} tens #{tens} ones #{ones}"
---
+++ the_basics_exercises_3.rb
@@ -0,0 +1,15 @@
+#1971 - "Hello"
+#2004 - "terminator"
+#2013 - "Shaji"
+#2001 - "Navi"
+#1981 - "Navya"
+
+movies = { :Hello => 1971, :terminator => 2004, :Shaji => 2013, :Navi => 2001, :Navya => 1981}
+puts movies[:Hello]
+puts movies[:terminator]
+puts movies[:Shaji]
+puts movies[:Navi]
+puts movies[:Navya]
+
+
+
